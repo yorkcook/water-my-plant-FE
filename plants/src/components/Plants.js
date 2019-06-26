216559df75ef9699c70352d58ea7a43ec9d4a1e2
York Cook <yorkcook@Johnny-Castle.lan1>
2019-06-25 20:13:39 -0400

    WIP

---
+++ plants/src/components/Plants.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Plants extends Component {
+  state = {
+    name: "",
+    description: "",
+    schedule: []
+  };
+
+  render() {
+    return <div />;
+  }
+}
+
+export default Plants;
